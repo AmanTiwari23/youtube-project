b2aaa11f1ebce2553a3a34787821da1a5aeb42cd
Aman Tiwari <tiwari95aman@gmail.com>
2024-10-23 19:27:45 +0530

    Develope the search suggestion

--- src/components/Head.js
+++ src/components/Head.js
@@ -5,16 +5,20 @@ import { YOUTUBE_SEARCH_API } from "../utils/constent";
 
 const Head = () => {
   const [searchQuery, setSearchQuery] = useState("");
-
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
   useEffect(() => {
-    console.log(searchQuery);
-    getSearchSugestions();
+    const timer = setTimeout(() => getSearchSugestions(), 200);
+
+    return () => {
+      clearTimeout(timer);
+    };
   }, [searchQuery]);
 
   const getSearchSugestions = async () => {
     const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
     const json = await data.json();
-    console.log(json[1]);
+    setSuggestions(json[1]);
   };
 
   const dispatch = useDispatch();
@@ -38,15 +42,30 @@ const Head = () => {
         />
       </div>
       <div className="col-span-10">
-        <input
-          type="text"
-          className="w-1/2 border border-gray-400 p-1 rounded-l-full"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
-        <button className="px-5 p-1 bg-gray-200 rounded-r-full border border-gray-400 border-l-0">
-          🔍
-        </button>
+        <div>
+          <input
+            type="text"
+            className="w-1/2 border border-gray-400 p-1 rounded-l-full"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onFocus={() => setShowSuggestions(true)}
+            onBlur={() => setShowSuggestions(false)}
+          />
+          <button className="px-5 p-1 bg-gray-200 rounded-r-full border border-gray-400 border-l-0">
+            🔍
+          </button>
+        </div>
+        {showSuggestions && (
+          <div className="fixed bg-white py-2 px-2 w-[35rem] shadow-lg rounded-lg z-10 border border-gray-50">
+            <ul>
+              {suggestions.map((s) => (
+                <li key={s} className="py-2 shadow-sm hover:bg-gray-200">
+                  🔍 {s}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
       <div className="col-span-1">
         <img
