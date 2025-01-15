2ebb317683d6a9926ad84c0be20690b26f27d286
Aman Tiwari <tiwari95aman@gmail.com>
2025-01-15 15:14:59 +0530

    fix video card

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -5,8 +5,8 @@ const VideoCard = ({info}) => {
     const {snippet, statistics} = info;
     const { channelTitle, title,thumbnails} = snippet;
   return (
-    <div className='p-2 m-4 w-80 shadow-lg rounded-lg hover:bg-gray-300'>
-      <img className='rounded-lg object-cover w-80 h-48 '  src={thumbnails.medium.url} alt='thumb nail'/>
+    <div className='p-2 m-4 w-72 shadow-lg rounded-lg hover:bg-gray-300'>
+      <img className='rounded-lg object-cover w-72 h-44 '  src={thumbnails.medium.url} alt='thumb nail'/>
       <ul>
         <li className='font-bold py-2 truncate ...'>{title}</li>
         <li>{channelTitle}</li>
